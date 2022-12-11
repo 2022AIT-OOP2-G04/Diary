f4a77a8928940aafad8dc95fd13da52317785f57
koron56 <63432884+koron56@users.noreply.github.com>
2022-12-11 13:18:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from diaries.MasudaDiary import MasudaDiary
 from diaries.HidetoDiary import HidetoDiary
 from diaries.SasakiDiary import SasakiDiary
 from diaries.FujiiDiary import FujiiDiary
-from diaries.ShiraiDiary import ShiraiDia
+from diaries.ShiraiDiary import ShiraiDiary
 from diaries.AgataDiary import AgataDiary
 
 #
